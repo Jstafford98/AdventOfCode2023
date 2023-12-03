4212d045c4990893b8e551e229a3c29a43eb3d6d
Jstafford98 <59448471+Jstafford98@users.noreply.github.com>
2023-12-02 20:11:50 -0500

    Further updates to day and add .gitignore

--- day1.py
+++ day1.py
@@ -6,13 +6,18 @@ PART_TWO_ANSWER = 54649
 DIGIT_MAP = {v:str(i) for i,v in enumerate(['one','two','three','four','five','six','seven','eight','nine'],1)}
 MAX_DEPTH = max(map(len, DIGIT_MAP))
 
-def extract_calibration_codes(s : str, max_depth : int) -> tuple[str, str] :
+def extract_calibration_codes(
+    s         : str, 
+    max_depth : int, 
+    digit_map : dict[str, int]
+) -> tuple[str, str] :
     ''' 
         extracts calibration codes for AOC Day 1 Pt. 2, 
         consider string spelling of digits in the solution as well 
         
         i.e 'two1nine' would become '219' and return 29
     '''
+    
     digits = []
     for i in range(0, len(s)):
         end_idx = min(i + max_depth + 1, len(s) + 1) # limit end idx
@@ -21,8 +26,8 @@ def extract_calibration_codes(s : str, max_depth : int) -> tuple[str, str] :
             if sub_str.isdigit():
                 digits.append(sub_str)
                 break
-            elif sub_str in DIGIT_MAP:
-                digits.append(DIGIT_MAP[sub_str])
+            elif sub_str in digit_map:
+                digits.append(digit_map[sub_str])
                 break
     return int(f'{digits[0]}{digits[-1]}')
 
@@ -33,6 +38,7 @@ def extract_calibration(s : str) -> int :
         
         i.e two1nine would become 1 and return 11
     '''
+    
     digits = list(filter(lambda x : x.isdigit(), s))
     return int(f'{digits[0]}{digits[-1]}')
 
@@ -44,7 +50,7 @@ if __name__ == '__main__':
     with open('day1.txt','r') as f:
         for x in f.readlines():
             digits_pt1.append(extract_calibration(x))
-            digits_pt2.append(extract_calibration_codes(x, max_depth=MAX_DEPTH))
+            digits_pt2.append(extract_calibration_codes(x, max_depth=MAX_DEPTH, digit_map=DIGIT_MAP))
 
     solution_pt1 = sum(digits_pt1)
     solution_pt2 = sum(digits_pt2)
